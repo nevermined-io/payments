79edfb8393c08947c65cb895ff085a34d05e1ef5
Rodolphe Marques <marques.rodolphe@gmail.com>
2025-09-11 18:11:50 +0200

    fix: tests

--- tests/unit/observability-api.test.ts
+++ tests/unit/observability-api.test.ts
@@ -1,9 +1,23 @@
 import { Payments } from '../../src/payments'
 import { Environments } from '../../src/environments'
+import { StartAgentRequest } from '../../src/common/types'
 
 describe('Observability-Api (unit)', () => {
   const nvmApiKeyHash =
     'eyJhbGciOiJFUzI1NksifQ.eyJpc3MiOiIweDA2OEVkMDBjRjA0NDFlNDgyOUQ5Nzg0ZkNCZTdiOWUyNkQ0QkQ4ZDAiLCJzdWIiOiIweDQzQ0FDZUQxNURhRWE4MDE2RTIzNDI5NThFZjgyQTk0NTAxMTJlMTgiLCJqdGkiOiIweDJmNzZlN2ZkYzI3ZWIyYmYzYjQ0ODRlYmY1Mzk4YWQ5MjllMjMxYTYwODlmMDE5Zjk5ZDAzMjk3Mjk4YWYzZTkiLCJleHAiOjE3ODczMjc3MDEsIm8xMXkiOiJzay1oZWxpY29uZS1tamZ6MzJhLXF4aXVpMnEteDV5YmNoeS11NnJjZXhpIn0.KdMMHlHvpENmvT6ozVdfmtNC6KFMnh_XNNQEV_qxPpFgzvUM86vM21E5YKFVWkjK_yMAnNh6XVJGVjqPVuew6xs'
+  const agentRequest = {
+    agentRequestId: 'test-agent-request-id',
+    agentName: 'test-agent-name',
+    agentId: 'test-agent-id',
+    balance: {
+      planId: 'test-plan-id',
+      planName: 'test-plan-name',
+      planType: 'test-plan-type',
+      pricePerCredit: 0.001,
+    },
+    urlMatching: 'test-url-matching',
+    verbMatching: 'test-verb-matching',
+  } as StartAgentRequest
 
   it('should initialize correctly with the helicone api key and helicone url', () => {
     const payments = Payments.getInstance({
@@ -11,7 +25,7 @@ describe('Observability-Api (unit)', () => {
       environment: 'custom',
     })
 
-    const cfg = payments.observability.withHeliconeOpenAI('sk-openai', {
+    const cfg = payments.observability.withHeliconeOpenAI('sk-openai', agentRequest, {
       agentid: 'test-agent',
       sessionid: 'test-session',
     })
@@ -30,7 +44,7 @@ describe('Observability-Api (unit)', () => {
     Environments.custom = { ...originalCustom, heliconeUrl: 'https://custom.helicone.com' }
 
     const payments = Payments.getInstance({ nvmApiKey: nvmApiKeyHash, environment: 'custom' })
-    const cfg = payments.observability.withHeliconeOpenAI('sk-openai', {
+    const cfg = payments.observability.withHeliconeOpenAI('sk-openai', agentRequest, {
       agentid: 'test-agent',
       sessionid: 'test-session',
     })
